56528b8f426025cc3268bb4932c75cb79837ba10
Lucemans <mrdisk@hotmail.com>
2020-06-28 08:52:42 +0200

    Create App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent, useState } from "react";
+
+export default class App extends React.Component {
+    render() {
+      return (<span>Hello World!</span>);
+    }
+}
